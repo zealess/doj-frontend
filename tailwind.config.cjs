6ca1dd998d74c588fcdb17868aa87e9941ca2287
zealess <alessandro.targaryen0@gmail.com>
2025-11-29 19:03:08 +0100

    modif

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
